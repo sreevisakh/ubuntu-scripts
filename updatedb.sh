b8f19ac997e6b774b096b07b88123bdde60cc8cd
Sreevisakh <mail@sreevisakh.com>
2014-11-14 21:59:57 +0530

    Added spotify install

--- updatedb.sh
+++ updatedb.sh
@@ -3,11 +3,11 @@ FILE="website_$(date +%b)_$(date +%d)_$(date +%Y).sql"
 P="/home/sv/scripts/db_backups/local/"
 Q="/home/sv/scripts/db_backups/"
 echo "Taking Backup of local DB"
-mysqldump --verbose -u presto -ppr3st0 "website"> $P$FILE
+mysqldump -h local.dev.creyate.com --verbose -u presto -ppr3st0 "website"> $P$FILE
 echo "Started downloading db from qa"
 mysqldump --verbose -h qa.creyate.com -u presto -ppr3st0 "website"> $Q$FILE
 echo "Create Dump from qa"
-mysql -u presto -ppr3st0 -e "
+mysql -h local.dev.creyate.com -u presto -ppr3st0 -e "
 DROP DATABASE website;
 CREATE DATABASE website;
 use website;
